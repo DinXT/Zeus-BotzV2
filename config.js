1acf8290755050587b054732fe1e2034fadb0dd4
Wibu <92778687+DinXT@users.noreply.github.com>
2022-03-22 10:18:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['6289625556161','62896255561613'] // Put your number here //owner eval
-global.kontak = ['6289625556161','62896255561613'] //Ketika ada yang ngetik #owner
-global.mods = ['0'] // Want some help?
-global.prems = ['6289625556161','601164215501','60166408295','601161197507','601156232697','601172962735','60182387590','60109211153','6281391552752'] // Premium user has unlimited limit
+global.linkGC = ['https://chat.whatsapp.com/L58vTWNHtz0EBmlwMWdTn5', 'https://chat.whatsapp.com/B5YsmkpgUf5AOFuR1bkYmx'] // ganti jadi group lu
+global.owner = ['6281380674817'] // Put your number here //owner eval
+global.kontak = ['6281380674817', '0'] //Ketika ada yang ngetik #owner
+global.mods = [''] // Want some help?
+global.prems = [''] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   rey: 'https://server-api-rey.herokuapp.com',
@@ -46,30 +46,30 @@ lolkey = 'RFK-Rey'
 zekskey = 'apivinz'
 xteamkey = 'Dawnfrostkey'
 //xteam        MIMINETBOT
-namaig = 'GAK ADA :)'
-namagithub = 'https://github.com/MASTERMAN200'
-kasihcaption = `Nih kak`
-namakontak1 = 'Pilar Owner Bot'
-namakontak2 = 'Caca Admin Bot'
+namaig = 'https://instagram.com/dinxt.xyz'
+namagithub = 'https://github.com/DinXT'
+kasihcaption = `Done.`
+namakontak1 = 'DinXT | Owner'
+namakontak2 = 'WhatsApp.'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'Zeus Botz' // ganti aja
-global.author = 'Zeus Botz' // ganti aja
+global.packname = 'DinXT' // ganti aja
+global.author = 'Sticker' // ganti aja
 
 //yyy
-bc = 'ZeusBotz' //ZeusBotz Broadcast
-footer = '\n© ZeusBotz'
-namabot = 'ZEUS BOTZ'
-namalu = 'PILAR'
-watermark = 'ZeusBotz' //ganti aja
+bc = '' //ZeusBotz Broadcast
+footer = '\n DinXT'
+namabot = 'WhatsApp Bot.'
+namalu = 'DinXT'
+watermark = 'DinXT' //ganti aja
 
 // 
-wait = '_*Tunggu Sebentar...*_'
-global.botwm = 'ZeusBotz'
-global.wait = '_*Tunggu Sebentar...*_'
-global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
+wait = '_*Please Wait...*_'
+global.botwm = 'DinXT'
+global.wait = '_*Please Wait...*_'
+global.rpg = 'Fitur Rpg Dimatikan\nSilahkan minta owner untuk mengaktifkan fitur ini !'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
 global.eror = '_*Server Error*_'
 
